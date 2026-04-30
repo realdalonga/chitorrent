dcad4c12aa9d71eaccbfdd91e73fcdc82cc94463
realdalonga <sebastiankontakt7@gmail.com>
2026-04-30 22:09:23 +0200

    Added clang-format configuration and pre commit hook.

--- include/decodedTorrent.h
+++ include/decodedTorrent.h
@@ -4,7 +4,6 @@ namespace chit {
 
 struct DecodedTorrent {
   size_t length;
-}
+};
 
-
-}
+}  // namespace chit
--- include/torrentParser.h
+++ include/torrentParser.h
@@ -1,17 +1,16 @@
 #pragma once
 #include <vector>
+
 #include "decodedTorrent.h"
 namespace chit {
 
 class TorrentFileParser {
-
-private:
+ private:
   static int decodeInt() const;
   static std::vector decodeList() const;
 
-public:
+ public:
   DecodedTorrent decode(const std::string&) const;
-
 };
- 
-}
+
+}  // namespace chit
