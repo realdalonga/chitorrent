1fc29220391271ace7c948f0071d332b37df7160
realdalonga <sebastiankontakt7@gmail.com>
2026-04-30 21:45:18 +0200

    Moved DecodedTorrent struct definition into sperate file. Added existing classes and types to the namespace of the chitorrent.

---
+++ include/decodedTorrent.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace chit {
+
+struct DecodedTorrent {
+  size_t length;
+}
+
+
+}
--- include/torrentParser.h
+++ include/torrentParser.h
@@ -1,8 +1,17 @@
-class TorrentParser {
+#pragma once
+#include <vector>
+#include "decodedTorrent.h"
+namespace chit {
+
+class TorrentFileParser {
+
+private:
+  static int decodeInt() const;
+  static std::vector decodeList() const;
+
 public:
   DecodedTorrent decode(const std::string&) const;
-};
 
-struct DecodedTorrent {
-  size_t length;
+};
+ 
 }
